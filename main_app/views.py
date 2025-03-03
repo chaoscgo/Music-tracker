132ad2364cd465b78b6ae5e2cc1b464e528a6df2
Beverly <chaoscgo@hotmail.com>
2025-03-03 14:24:06 -0600

    Added starter about page

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
-def home(request):
-    
+def home(request):    
     return HttpResponse('<h1>Hello World!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About the Music Tracker</h1>')
