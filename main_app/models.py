aa05e5786c91ad2fb0c3b7eca4cd9ced15ddc0fa
Beverly <chaoscgo@hotmail.com>
2025-03-04 13:31:16 -0600

    added Song model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    name = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    date = models.DateField('Date Published')
+    length = models.IntegerField('Length of Song')
+
+    def __str__(self):
+        return self.name
