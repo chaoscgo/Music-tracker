3067a0333ee05eb12585a10a29eccbdf4f8aadcb
Beverly <chaoscgo@hotmail.com>
2025-03-04 13:56:13 -0600

    created Song model and database

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,5 @@
 from django.shortcuts import render
-
-class Song:
-    def __init__(self, name, artist, genre, date, length):
-        self.name = name
-        self.artist = artist
-        self.genre = genre
-        self.date = date
-        self.length = length
-
-songs = [
-    Song("Dragula", 'Rob Zombie', 'Hard Rock', '8/24/1998', 4),
-    Song("She's Been Through the Fire", 'Brian Rhea', 'Country', '6/10/2022', 4),
-    Song("Love the Hell Out of You", 'Lewis Capaldi', 'Alternative', '5/19/2023', 3),
-    Song("Used to Be Young", 'Miley Cyrus', 'Pop', '8/25/2023', 3),
-]
-
+from .models import Song
 
 def home(request):    
     return render(request, 'home.html')
@@ -23,4 +8,5 @@ def about(request):
     return render(request, 'about.html')
 
 def song_index(request):
+    songs = Song.objects.all()
     return render(request, 'songs/index.html', {'songs': songs})
