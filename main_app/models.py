51f6c6040dbdb458fe32b0c2bc235c027203247b
nichoOtero <nichonaks@gmail.com>
2025-03-07 09:43:10 -0600

    finished authentication

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Song(models.Model):
     name = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
     date = models.DateField()
     length = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Song
 from .forms import ArtistForm
+from django.contrib.auth.views import LoginView
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('song-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
 
 def home(request):    
     return render(request, 'home.html')
@@ -9,15 +28,18 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def song_index(request):
-    songs = Song.objects.all()
+    songs = Song.objects.filter(user=request.user)
     return render(request, 'songs/index.html', {'songs': songs})
 
+@login_required
 def song_detail(request, song_id):
     song = Song.objects.get(id=song_id)
     artist_form = ArtistForm()
     return render(request, 'songs/detail.html', {'song': song, 'artist_form': artist_form})
 
+@login_required
 def add_artist(request, song_id):
     form = ArtistForm(request.POST)
     if form.is_valid():
@@ -26,15 +48,22 @@ def add_artist(request, song_id):
         new_artist.save()
     return redirect('song-detail', song_id=song_id)
 
-class SongCreate(CreateView):
+class Home(LoginView):
+    template_name = 'home.html'
+
+class SongCreate(LoginRequiredMixin, CreateView):
     model = Song
     fields = ['name', 'genre', 'date', 'length']
 
-class SongUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class SongUpdate(LoginRequiredMixin, UpdateView):
     model = Song
     fields = ['name', 'genre', 'date', 'length']
 
-class SongDelete(DeleteView):
+class SongDelete(LoginRequiredMixin, DeleteView):
     model = Song
     success_url = '/songs/'
 
